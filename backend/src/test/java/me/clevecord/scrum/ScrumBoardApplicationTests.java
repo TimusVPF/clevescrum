bc1917973ca7dda7fd2423adf1e0e66937f9ea14
Minazuki <tenkoirigo@gmail.com>
2020-04-03 14:39:58 +0900

    Init commit

---
+++ backend/src/test/java/me/clevecord/scrum/ScrumBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.clevecord.scrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
